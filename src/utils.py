81be7e4be6014ca78ea790d975f7de35db9d91dc
Andrei Tihomirov <moon200505@mail.ru>
2025-07-13 18:56:25 +0300

    utils.py for views.py DONE

--- src/utils.py
+++ src/utils.py
@@ -1,66 +1,84 @@
+import datetime
 import json
+import os
+import urllib
+from typing import Any
 
 import pandas as pd
-from typing import Any
-import os
-import datetime
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("API_KEY")
+api_key_for_currency = os.getenv("API_KEY_for_currency")
+api_key_for_stocks = os.getenv("API_KEY_for_stocks")
 
-PATH_TO_EXCEL = os.path.join(os.path.dirname(__file__), '../data', 'operations.xlsx')
-PATH_TO_JSON = os.path.join(os.path.dirname(__file__), '../user_settings.json')
+PATH_TO_EXCEL = os.path.join(os.path.dirname(__file__), "../data", "operations.xlsx")
+PATH_TO_JSON = os.path.join(os.path.dirname(__file__), "../user_settings.json")
+
 
 def json_settings_for_currency(file_path: str) -> Any:
     """Функция принимает ПУТЬ к файлу JSON и преобразует его в Python список возвращая значения требуемых валют
     из файла настроек user_settings.json"""
     with open(file_path, "r", encoding="utf-8") as file:
         python_data = json.load(file)
-        currency_list  = python_data.get('user_currencies')
+        currency_list = python_data.get("user_currencies")
     return currency_list
+
 
 def json_settings_for_stocks(file_path: str) -> Any:
     """Функция принимает ПУТЬ к файлу JSON и преобразует его в Python список возвращая значения требуемых акций
     из файла настроек user_settings.json"""
     with open(file_path, "r", encoding="utf-8") as file:
         python_data = json.load(file)
-        stocks_list  = python_data.get('user_currencies')
+        stocks_list = python_data.get("user_stocks")
     return stocks_list
 
 
-def get_currency(currency_list):
+def get_currency(currency_list: list) -> Any:
     """Функция принимает на вход список из двух валют требуемых для нахождения курсов с apilayer.com
-     и возвращает словарь 'Валюта: курс в рублях' """
+    и возвращает словарь 'Валюта: курс в рублях'"""
     url_x = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency_list[0]}"
     url_y = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency_list[1]}"
-    payload = {}
-    headers = {
-        "apikey": api_key
-    }
+    payload: dict = {}
+    headers = {"apikey": api_key_for_currency}
     response_x = requests.request("GET", url_x, headers=headers, data=payload)
     result_x = response_x.json()
     response_y = requests.request("GET", url_y, headers=headers, data=payload)
     result_y = response_y.json()
 
-    return {currency_list[0]:result_x['rates']['RUB'], currency_list[1]:result_y['rates']['RUB']}
+    return {
+        currency_list[0]: result_x["rates"]["RUB"],
+        currency_list[1]: result_y["rates"]["RUB"],
+    }
 
-print(get_currency(json_settings_for_currency(PATH_TO_JSON)))
+
+def get_stocks_data(stocks_list: list) -> Any:
+    """Функция принимает на вход список требуемых для поиска Акций и возвращает
+    словарь в виде 'Акция': 'Стоимость'"""
+    data = []
+    result = {}
+    for stock in stocks_list:
+        url = f"https://financialmodelingprep.com/stable/quote-short?symbol={stock}&apikey={api_key_for_stocks}"
+        response = urllib.request.urlopen(url)
+        json_data = json.loads(response.read().decode("utf-8"))
+        data.append(json_data[0])
+    for i in data:
+        result[i["symbol"]] = i["price"]
+    return result
 
 
-def greetings(date: str)->str:
+def greetings(date: str) -> str:
     """Функция принимает на вход Дату в виде строки, преобразует в объект времени
-    и возвращает """
-    date_object = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    и возвращает"""
+    date_object = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     if 5 <= date_object.hour < 11:
-        return 'Доброе утро'
+        return "Доброе утро"
     elif 11 <= date_object.hour < 16:
-        return 'Добрый день'
+        return "Добрый день"
     elif 16 <= date_object.hour < 23:
-        return 'Добрый вечер'
+        return "Добрый вечер"
     else:
-        return 'Доброй ночи'
+        return "Доброй ночи"
 
 
 def excel_to_df(file_path: str):
@@ -69,34 +87,45 @@ def excel_to_df(file_path: str):
     return df
 
 
-def filter_by_date(df, end_date: str)-> Any:
+def filter_by_date(df, user_date: str) -> pd.DataFrame:
     """Функция принимает на вход дату и
-    фильтрует DataFrame в диапазоне принимаемой даты с начал месяца этой самой даты блин"""
-    df['Дата платежа'] = pd.to_datetime(df['Дата платежа'], dayfirst=True)
-    end_date = pd.to_datetime(end_date)
+    фильтрует DataFrame в диапазоне принимаемой даты с начал месяца этой самой даты блин
+    """
+    df["Дата платежа"] = pd.to_datetime(df["Дата платежа"], dayfirst=True)
+    end_date = pd.to_datetime(user_date)
     start_of_month = end_date.replace(day=1)
-    return df[(df['Дата платежа'] >= start_of_month) &
-              (df['Дата платежа'] <= end_date)]
+    return df[(df["Дата платежа"] >= start_of_month) & (df["Дата платежа"] <= end_date)]
 
 
 def total_spent(df) -> Any:
-    """Функция принимает на вход Excel файл и с помощью pandas фильтрует по 'Статус': 'OK/Failed',
-     фильтрует по тратам, группирует по 'Номер карты' и возвращает словарь, где
-     ключ: номер карты, значение: сумма всех расходов"""
-    df = pd.read_excel(df)
-    df_state = df[df['Статус'] == 'OK']
-    df_negative = df_state[df_state['Сумма платежа'] < 0]
-    card_name_grouped = df_negative.groupby('Номер карты')
-    sum_price_by_card_number = card_name_grouped['Сумма платежа'].sum()
+    """Функция принимает на вход DataFrame и с помощью pandas фильтрует по 'Статус': 'OK/Failed',
+    фильтрует по тратам, группирует по 'Номер карты' и возвращает словарь, где
+    ключ: номер карты, значение: сумма всех расходов"""
+    df_state = df[df["Статус"] == "OK"]
+    df_negative = df_state[df_state["Сумма платежа"] < 0]
+    card_name_grouped = df_negative.groupby("Номер карты")
+    sum_price_by_card_number = card_name_grouped["Сумма платежа"].sum()
     total_spent_dict = sum_price_by_card_number.to_dict()
     return total_spent_dict
 
+
+def top_transactions(df) -> Any:
+    """Функция принимает на вход DataFrame и с помощью pandas фильтрует по 'Статус': 'OK/Failed',
+    фильтрует по тратам и возвращает словарь ТОП-5 транзакций по сумме платежа в DF"""
+    df_state = df[df["Статус"] == "OK"]
+    df_negative = df_state[df_state["Сумма платежа"] < 0]
+    df_sort = df_negative.sort_values(by="Сумма платежа", ascending=True)
+    df_sort_five = df_sort.head(5)
+    top_five = df_sort_five.to_dict("records")
+    return top_five
+
+
+# print((top_transactions(filter_by_date((excel_to_df(PATH_TO_EXCEL)),end_date = '2021-12-15')))[0])
+
+
 def last_four_card_numbers(card_number: str) -> str:
-    """Функция, которая принимает на вход номер карты и возвращает последние 4 цифры номера """
+    """Функция, которая принимает на вход номер карты и возвращает последние 4 цифры номера"""
     return card_number[-4:]
-
-
-
 
 
 # def excel_to_python_data(file_path: str) -> Any:
